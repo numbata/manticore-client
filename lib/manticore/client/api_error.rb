01efe46633899641cc257ad6e20e083ba547fe83
Andrey Subbota <subbota@gmail.com>
2026-03-07 11:05:04 +0100

    Replace arbitrary instance_variable_set with explicit attributes in ApiError
    Only set known attributes (code, response_headers, response_body)
    instead of blindly setting any key from the options hash as an
    instance variable.

--- lib/manticore/client/api_error.rb
+++ lib/manticore/client/api_error.rb
@@ -27,9 +27,9 @@ module ManticoreClient::Client
           super arg
         end
 
-        arg.each do |k, v|
-          instance_variable_set "@#{k}", v
-        end
+        @code = arg[:code] || arg['code']
+        @response_headers = arg[:response_headers] || arg['response_headers']
+        @response_body = arg[:response_body] || arg['response_body']
       else
         super arg
         @message = arg
