96dea1161ecefa23e0185690a1360aeb4b2517d0
Andrey Subbota <subbota@gmail.com>
2026-03-07 16:15:19 +0100

    Revert Indexer memoization for thread safety
    Memoizing Indexer on the class shares a Faraday connection across
    threads in multi-threaded servers like Puma. Creating a new Indexer
    per call is cheap and avoids the race condition.

--- lib/manticore-rails/searchable.rb
+++ lib/manticore-rails/searchable.rb
@@ -30,7 +30,7 @@ module ManticoreRails
       end
 
       def manticore_indexer
-        @manticore_indexer ||= Indexer.new(manticore_index)
+        Indexer.new(manticore_index)
       end
 
       def search(query, options = {})
